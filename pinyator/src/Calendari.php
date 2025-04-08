22058b8d5e63e4d5a4017b50e7909fb41f6f00a3
bielgomis <bielgomis2018@gmail.com>
2025-04-08 15:47:01 +0200

    Canvia title Castell

--- pinyator/src/Calendari.php
+++ pinyator/src/Calendari.php
@@ -17,11 +17,7 @@
 				<div class="entry-content">				
 					<section id="builder-section-1449513204629" class="builder-section builder-section-first builder-section-text builder-section-last builder-text-columns-1">
 						<div class="builder-section-content">
-							<div class="builder-text-column builder-text-column-1" id="builder-section-1449513204629-column-1">
-								<div class="builder-text-content">
-									<p><iframe style="border-width: 0;" src="https://www.google.com/calendar/embed?title=Marrecs%20de%20Salt&amp;showTitle=0&amp;showTz=0&amp;height=600&amp;wkst=2&amp;hl=ca&amp;bgcolor=%23FFFFFF&amp;src=73kifkbcsfmrn658s1g5709ou8%40group.calendar.google.com&amp;color=%23853104&amp;src=5ibslpp488u4fclljo7p07pn10%40group.calendar.google.com&amp;color=%232952A3&amp;src=ampkdehn8sjot8phqqd2soaat8%40group.calendar.google.com&amp;color=%238C500B&amp;src=jnmds6shtf4p6havqa0494vbt4%40group.calendar.google.com&amp;color=%23875509&amp;src=webmarrecs%40gmail.com&amp;color=%23182C57&amp;src=ot3k4tvrqn6qfmart5q2e929u0%40group.calendar.google.com&amp;color=%23182C57&amp;ctz=Europe%2FAndorra" width="920" height="600" frameborder="0" scrolling="no"></iframe></p>
-								</div>
-							</div>
+							
 						</div>
 					</section>
 				</div>		
